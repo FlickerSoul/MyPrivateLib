3e796fe9c2adcd967a97b0d6402a5248aff7b1eb
Heyuan Zeng <hi@universe.observer>
2025-04-23 23:46:51 +0100

    Add content

--- Sources/MyPrivateLib/MyPrivateLib.swift
+++ Sources/MyPrivateLib/MyPrivateLib.swift
@@ -1,2 +1,7 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+func mySecretFunction() -> Int {
+    return Int.random(in: 1 ... 100)
+}
+
+public func myPublicFunction() -> Int {
+    mySecretFunction()
+}
